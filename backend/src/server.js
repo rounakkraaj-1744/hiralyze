1676744bb6fad4beb4468caa0b2ffb73810d6801
rounakkraaj-1744 <rounakkraaj707@gmail.com>
2025-07-05 19:40:31 +0530

    express backend setup

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+const app = express()
+const port = process.env.PORT || 8080
+dotenv.config()
+
+app.use (cors())
+
+app.get ("/", (req, res)=>{
+    res.send ("Server started!!")
+})
+
+app.listen (port, ()=>{
+    console.log (`Server started at http://localhost:${port}`)
+})
